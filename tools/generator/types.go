744090fddee43eb91142d30cccb8bd3aa5151431
ywn <runningerror@qq.com>
2025-01-10 11:32:04 +0800

    tools

--- tools/generator/types.go
+++ tools/generator/types.go
@@ -2,21 +2,21 @@ package generator
 
 // Field 表示模型字段
 type Field struct {
-	Name    string // 字段名
-	Type    string // 字段类型
-	Tag     string // 字段标签
-	Comment string // 字段注释
+	Name    string `yaml:"name"`    // 字段名
+	Type    string `yaml:"type"`    // 字段类型
+	Tag     string `yaml:"tag"`     // 字段标签
+	Comment string `yaml:"comment"` // 字段注释
 }
 
 // Entity 表示实体定义
 type Entity struct {
-	Name      string  // 实体名称
-	TableName string  // 表名
-	Fields    []Field // 字段列表
+	Name      string  `yaml:"name"`       // 实体名称
+	TableName string  `yaml:"table_name"` // 表名
+	Fields    []Field `yaml:"fields"`     // 字段列表
 }
 
 // ModuleConfig 表示模块配置
 type ModuleConfig struct {
-	Module   string   // 模块名称
-	Entities []Entity // 实体列表
+	Module   string   `yaml:"module"`   // 模块名称
+	Entities []Entity `yaml:"entities"` // 实体列表
 }
