58f14b1d758fa60670056af82a2805e51fc359bb
ywn <runningerror@qq.com>
2025-02-06 14:52:09 +0800

    限流中间件

--- apps/admin/internal/transport/api_http.go
+++ apps/admin/internal/transport/api_http.go
@@ -4,12 +4,13 @@ import (
 	"fiber_web/apps/admin/internal/endpoint"
 	"fiber_web/apps/admin/internal/middleware"
 	"github.com/gofiber/fiber/v2"
+	"time"
 )
 
 func RegisterApiHttp(app fiber.Router, handlers *endpoint.Handlers) {
 	app.Post("/register", handlers.User.Register)
 	app.Post("/login", handlers.User.Login)
-	app.Post("/refresh-token", middleware.Jwt(), handlers.User.RefreshToken)
+	app.Post("/refresh-token", middleware.Jwt(), middleware.RateLimit(3, time.Minute), handlers.User.RefreshToken)
 	app.Get("/users", middleware.Jwt(), middleware.Pagination(), handlers.User.ListUsers)
 	app.Get("/test", middleware.Jwt(), middleware.Pagination(), handlers.User.TestUser)
 	app.Get("/users/me", middleware.Jwt(), handlers.User.GetProfile)
