55d8daab4f1074eeda3f0ce343135fc68c24ccea
ywn <runningerror@qq.com>
2025-03-07 14:21:07 +0800

    1

--- apps/admin/internal/transport/api_http.go
+++ apps/admin/internal/transport/api_http.go
@@ -11,7 +11,7 @@ func RegisterApiHttp(app fiber.Router, handlers *endpoint.Handlers) {
 	app.Post("/register", handlers.User.Register)
 	app.Post("/login", handlers.User.Login)
 	app.Post("/refresh-token", middleware.Jwt(), middleware.RateLimit(3, time.Minute), handlers.User.RefreshToken)
-	app.Get("/users", middleware.Jwt(), middleware.Pagination(), handlers.User.ListUsers)
+	app.Get("/users", middleware.Jwt(), middleware.Pagination(), handlers.User.List)
 	app.Get("/test", middleware.Jwt(), middleware.Pagination(), handlers.User.TestUser)
 	app.Get("/users/me", middleware.Jwt(), handlers.User.GetProfile)
 }
