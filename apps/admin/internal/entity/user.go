82610aa52406ef1005b468ce60bee26de7862215
ywn <runningerror@qq.com>
2025-03-07 14:29:15 +0800

    search

--- apps/admin/internal/entity/user.go
+++ apps/admin/internal/entity/user.go
@@ -6,7 +6,13 @@ type User struct {
 	ID        uint      `json:"id"`
 	Username  string    `json:"username"`
 	Email     string    `json:"email"`
+	Role      string    `json:"role"`
+	Status    int8      `json:"status"`
 	Password  string    `json:"-"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
+}
+
+func (u *User) TableName() string {
+	return "users"
 }
